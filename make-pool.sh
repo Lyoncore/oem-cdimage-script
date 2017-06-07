ba4246ad8fb2f46c1de4bbf84fd38c8da1868821
Woodrow Shen <woodrow.shen@canonical.com>
2017-06-07 11:11:11 +0800

    Fix the wrong path for returning debian-cd

--- make-pool.sh
+++ make-pool.sh
@@ -1,4 +1,6 @@
-#!/bin/sh
-mkdir -p $1/pool/extras
-cd $1/pool/extras
+#!/bin/bash
+mkdir -p ../oem-cdimage-script/pool/extras
+PCWD=$PWD
+cd ../oem-cdimage-script/pool/extras
 apt-get download tree htop vim
+cd $PCWD
