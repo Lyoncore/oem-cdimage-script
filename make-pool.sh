a89641adf91fb44a9018638ee4d7323e2a44d339
Woodrow Shen <woodrow.shen@canonical.com>
2017-11-17 10:41:22 +0800

    Add a feature to provide HWE 16.04 kernel installation

--- make-pool.sh
+++ make-pool.sh
@@ -3,4 +3,19 @@ mkdir -p ../oem-cdimage-script/pool/extras
 PCWD=$PWD
 cd ../oem-cdimage-script/pool/extras
 apt-get download tree htop vim
+#hwe 16.04
+if [ "$OEM_HWE_1604" = "1" ] ; then
+	apt-get download linux-firmware
+	apt-get download linux-generic-hwe-16.04
+	apt-get download linux-headers-4.10.0-38
+	apt-get download linux-headers-4.10.0-38-generic
+	apt-get download linux-headers-generic-hwe-16.04
+	apt-get download linux-image-4.10.0-38-generic
+	apt-get download linux-image-extra-4.10.0-38-generic
+	apt-get download linux-image-generic-hwe-16.04
+	apt-get download linux-signed-generic-hwe-16.04
+	apt-get download linux-signed-image-4.10.0-38-generic
+	apt-get download linux-signed-image-generic-hwe-16.04
+fi
+
 cd $PCWD
