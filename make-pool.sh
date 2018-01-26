7adde4509f83004559d9799c597299ed66859737
Woodrow Shen <woodrow.shen@canonical.com>
2018-01-26 05:41:14 +0000

    Update the kernel version to 4.13.0-31 for hwe

--- make-pool.sh
+++ make-pool.sh
@@ -7,14 +7,14 @@ apt-get download tree htop vim
 if [ "$OEM_HWE_KERNEL" = "generic-hwe" ] ; then
 	apt-get download linux-firmware
 	apt-get download linux-generic-hwe-16.04
-	apt-get download linux-headers-4.10.0-38
-	apt-get download linux-headers-4.10.0-38-generic
+	apt-get download linux-headers-4.13.0-31
+	apt-get download linux-headers-4.13.0-31-generic
 	apt-get download linux-headers-generic-hwe-16.04
-	apt-get download linux-image-4.10.0-38-generic
-	apt-get download linux-image-extra-4.10.0-38-generic
+	apt-get download linux-image-4.13.0-31-generic
+	apt-get download linux-image-extra-4.13.0-31-generic
 	apt-get download linux-image-generic-hwe-16.04
 	apt-get download linux-signed-generic-hwe-16.04
-	apt-get download linux-signed-image-4.10.0-38-generic
+	apt-get download linux-signed-image-4.13.0-31-generic
 	apt-get download linux-signed-image-generic-hwe-16.04
 fi
 
