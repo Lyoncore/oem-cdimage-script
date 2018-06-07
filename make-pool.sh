b8f7dd53ee475a62b80d8e01e094a424d9ee32e5
Woodrow Shen <woodrow.shen@canonical.com>
2018-06-07 12:26:21 +0800

    Remove deprecated customization

--- make-pool.sh
+++ make-pool.sh
@@ -2,19 +2,6 @@
 mkdir -p ../oem-cdimage-script/pool/extras
 PCWD=$PWD
 cd ../oem-cdimage-script/pool/extras
-#hwe kernel for 16.04
-if [ "$OEM_HWE_KERNEL" = "generic-hwe" ] ; then
-	apt-get download linux-firmware
-	apt-get download linux-generic-hwe-16.04
-	apt-get download linux-headers-4.13.0-31
-	apt-get download linux-headers-4.13.0-31-generic
-	apt-get download linux-headers-generic-hwe-16.04
-	apt-get download linux-image-4.13.0-31-generic
-	apt-get download linux-image-extra-4.13.0-31-generic
-	apt-get download linux-image-generic-hwe-16.04
-	apt-get download linux-signed-generic-hwe-16.04
-	apt-get download linux-signed-image-4.13.0-31-generic
-	apt-get download linux-signed-image-generic-hwe-16.04
-fi
+#apt-get download tree htop vim
 
 cd $PCWD
